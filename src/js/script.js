0d66f876d2568d1ddb0191adf34773bfc35ed159
nicolas vonner <nicolas.vonner@icloud.com>
2022-09-01 23:57:09 +0200

    Import du module custom, préparation des zones de sélection, nettoyage de l'interface

--- src/js/script.js
+++ src/js/script.js
@@ -1,9 +1,8 @@
 import "../css/style.css";
 import browser from "../assets/images/Crystal_Clear_app_linneighborhood.png";
-import * as handTrack from 'handtrackjs';
+import * as handTrack from './Modules/handtrackjs';
 
 let root = document.getElementById("root");
-let updateNote = document.getElementById("updatenote");
 let video;
 let canvas;
 let context;
@@ -11,17 +10,19 @@ let isVideo = false;
 let model = null;
 
 // https://victordibia.com/handtrack.js/#/
-
+// document.getElementById("Canvas").addEventListener("click", ()=>{
+// handTrack.stopVideo
+// });
 const defaultParams = {
-    flipHorizontal: false,
+    flipHorizontal: true,
     outputStride: 16,
-    imageScaleFactor: 1,
+    imageScaleFactor: 2,
     maxNumBoxes: 20,
     iouThreshold: 0.2,
     scoreThreshold: 0.6,
     modelType: "ssd320fpnlite",
     modelSize: "large",
-    bboxLineWidth: "2",
+    bboxLineWidth: "1",
     fontSize: 17,
 };
 
@@ -29,7 +30,6 @@ const defaultParams = {
 handTrack.load(defaultParams).then(lmodel => {
     // detect objects in the image.
     model = lmodel;
-    updateNote.innerText = "Loaded Model!"
     document.getElementById("video-button").disabled = false
 });
 
@@ -40,10 +40,6 @@ root.appendChild(h1);
 let img = document.createElement("img");
 img.src = browser;
 root.appendChild(img);
-
-document.getElementById("video-button").addEventListener("click",()=>{
-    launchWebcamVideo();
-});
 
 const launchWebcamVideo = () => {
     root.innerHTML = "";
@@ -70,23 +66,80 @@ const startVideo = () => {
     handTrack.startVideo(video).then((status) => {
         if (status)
         {
-            updateNote.innerText = "Video started. Now tracking";
             isVideo = true;
             runDetection();
         }
         else
         {
-            updateNote.innerText = "Error while starting the video";
+            //Error while starting the video;
         }
     });
 };
 
 function runDetection() {
     model.detect(video).then(predictions => {
-        console.log("Predictions: ", predictions);
+        //console.log("Predictions: ", predictions);
         model.renderPredictions(predictions, canvas, context, video);
+        // x0, y0, ?width,?heigth 
+        if(predictions[0] != undefined && predictions[0].label == "open"){
+            if(predictions[0].bbox[0]  < 200 && predictions[0].bbox[1]  > 310 ){
+                console.log("Y===>");
+                console.log(predictions[0].bbox[1]);
+                console.log("VIVES LES CHAMPIGONS 0")
+            }else{
+
+            }
+        }
+        if(predictions[1] != undefined && predictions[1].label == "open"){
+            if(predictions[1].bbox[0]  < 200 && predictions[1].bbox[1]  > 310 ){
+                console.log("Y===>");
+                console.log(predictions[1].bbox[1]);
+                console.log("VIVES LES CHAMPIGONS 1")
+            }else{
+
+            }
+        }
+        if(predictions[2] != undefined && predictions[2].label == "open"){
+            if(predictions[2].bbox[0]  < 200 ){
+                console.log("VIVES LES CHAMPIGONS 2")
+            }else{
+
+            }
+        }     
+        if(predictions[3] != undefined && predictions[3].label == "open"){
+            if(predictions[3].bbox[0]  < 200 ){
+                console.log("VIVES LES CHAMPIGONS 3")
+            }else{
+
+            }
+        }
+        if(predictions[4] != undefined && predictions[4].label == "open"){
+            if(predictions[4].bbox[0]  < 200 ){
+                console.log("VIVES LES CHAMPIGONS 4")
+            }else{
+
+            }
+        }
+        if(predictions[5] != undefined && predictions[5].label == "open"){
+            if(predictions[5].bbox[0]  < 200 ){
+                console.log("VIVES LES CHAMPIGONS 5")
+            }else{
+
+            }
+        }
+        if(predictions[6] != undefined && predictions[6].label == "open"){
+            if(predictions[6].bbox[0]  < 200 ){
+                console.log("VIVES LES CHAMPIGONS 6")
+            }else{
+
+            }
+        }
+
         if (isVideo) {
             requestAnimationFrame(runDetection);
         }
     });
 }
+
+//On lance la caméra
+launchWebcamVideo();
